2c2e520dffdbf0bd2367f7c6a22befcd31b68992
Ravnish Gupta <ravnish@gmail.com>
2022-02-19 16:17:44 -0800

    added count down timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,62 @@
 var questions = {
-    question1 ={
+    question1 : {
         text: "What are variables used for in JavaScript Programs?",
-        choice1 = "Storing numbers, dates, or other values",
-        choice2 = "Varying randomly",
-        choice3 = "Causing high-school algebra flashbacks",
-        choice4 = "None of the above",
-        correctAnswer = this.choice1
+        choice1: "Storing numbers, dates, or other values",
+        choice2: "Varying randomly",
+        choice3: "Causing high-school algebra flashbacks",
+        choice4: "None of the above",
+        correctAnswer: this.choice1
      },
-     question2 ={
+     question2 :{
         text: "Inside which HTML element do we put the JavaScript?",
-        choice1 = "<js>",
-        choice2 = "<scripting>",
-        choice3 = "<script>",
-        choice4 = "<javscript>",
-        correctAnswer = this.choice3
+        choice1: "<js>",
+        choice2: "<scripting>",
+        choice3: "<script>",
+        choice4: "<javscript>",
+        correctAnswer : this.choice3
      },
-     question3 ={
+     question3 :{
         text: "Which of the following is the structure of an if statement?",
-        choice1 = "if (conditional expression is true) thenexecute this codeend if",
-        choice2 = "if (conditional expression is true)execute this codeend if",
-        choice3 = "if (conditional expression is true)   {then execute this code>->}",
-        choice4 = "if (conditional expression is true) then {execute this code}",
-        correctAnswer = this.choice3
+        choice1: "if (conditional expression is true) thenexecute this codeend if",
+        choice2: "if (conditional expression is true)execute this codeend if",
+        choice3: "if (conditional expression is true)   {then execute this code>->}",
+        choice4: "if (conditional expression is true) then {execute this code}",
+        correctAnswer: this.choice3
      },
-     question4 ={
+     question4 :{
         text: "What is meant by 'this' keyword in javascript?",
-        choice1 = "It refers current object",
-        choice2 = "It referes previous object",
-        choice3 = "It is variable which contains value",
-        choice4 = "None of the above",
-        correctAnswer = this.choice1
+        choice1: "It refers current object",
+        choice2: "It referes previous object",
+        choice3: "It is variable which contains value",
+        choice4: "None of the above",
+        correctAnswer: this.choice1
      },
-     question5 ={
+     question5 :{
         text: "Which of the following is not JavaScript Data Types?",
-        choice1 = "Undefined",
-        choice2 = "Number",
-        choice3 = "Boolean",
-        choice4 = "Float",
-        correctAnswer = this.choice1
+        choice1: "Undefined",
+        choice2: "Number",
+        choice3: "Boolean",
+        choice4: "Float",
+        correctAnswer: this.choice1
      }
-}
+};
+
+var btn = document.getElementById("start");
+var timerEl = document.getElementById("time");
+btn.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    var timeLeft = 75;
+  
+    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function() {
+      timerEl.textContent = timeLeft;
+      
+      if (timeLeft === 0) {
+        clearInterval(timeInterval);
+        displayMessage();
+      }
+      timeLeft--;
+      //displayMessage();
+    },1000);
+  }
